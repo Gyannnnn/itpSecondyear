4dbabafd0dbb33539684c476ac7eba54b57dea44
gyanaranjan <hi.gyanaranjanpatra@gmail.com>
2025-02-11 20:36:58 +0530

    feat; / route

--- dist/index.js
+++ dist/index.js
@@ -14,6 +14,13 @@ app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 //ROUTES
 app.use("/api/v1/students", students_routes_1.default);
+app.get("/", (req, res) => {
+    res.json({
+        Message: "Welcome to Regestration Form api",
+        version: "1.0.0",
+        dev: "Gyanaranjan Patra"
+    });
+});
 app.listen(process.env.PORT || 3001, () => {
     console.log(`Server is running at http://localhost:${process.env.PORT || 3001}`);
 });
--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,13 @@ app.use(express.json());
 //ROUTES
 app.use("/api/v1/students",studentRouter);
 
-
+app.get("/",(req,res)=>{
+    res.json({
+        Message: "Welcome to Regestration Form api",
+        version:"1.0.0",
+        dev:"Gyanaranjan Patra"
+    })
+})
 
 app.listen(process.env.PORT || 3001,()=>{
     console.log(`Server is running at http://localhost:${process.env.PORT || 3001}`)
